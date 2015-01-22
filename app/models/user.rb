a911cb5c4806d800988d5c3ecb79559ad7906332
seekmas <seekmas@msn.cn>
2015-01-22 14:47:41 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :encryptable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
   devise :omniauthable, :omniauth_providers => [:facebook]
 
 end
