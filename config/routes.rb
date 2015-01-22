506e61e12a28d0db40bdfb9662d4668e94d203f5
seekmas <seekmas@msn.cn>
2015-01-22 15:39:44 +0800

    add facebook fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :courses
 
   resources :catalogs
-  
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
