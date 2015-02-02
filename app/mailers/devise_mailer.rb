b5f29ae4e212d4a049f6712948eabd2950ff6d11
seekmas <seekmas@msn.cn>
2015-02-02 13:38:58 +0800

    add utf-8

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class DeviseMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
