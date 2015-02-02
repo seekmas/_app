559ab86c361443894e6a1d78b709e750db373683
seekmas <seekmas@msn.cn>
2015-02-02 13:47:58 +0800

    update app#index

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -3,9 +3,9 @@ class AppController < ApplicationController
   before_action :authenticate_user! , only:[:history]
 
   def index
-    if current_user
-      UserNotifier.send_signup_email(current_user).deliver
-    end
+    # if current_user
+    #   UserNotifier.send_signup_email(current_user).deliver
+    # end
     @courses = Course.where(:enabled => true).paginate(:page => params[:page], :per_page => 6).all
   end
 
