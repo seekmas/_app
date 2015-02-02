59a085508cfef6de0aecd18b488d16cad5c6806e
seekmas <seekmas@msn.cn>
2015-02-02 13:35:49 +0800

    add zh-CN devise and mail nitify

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :encryptable,
+  devise :database_authenticatable, :registerable, :confirmable, :encryptable,
          :recoverable, :rememberable, :trackable, :validatable
 
   devise :omniauthable, :omniauth_providers => [:facebook]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129063221) do
+ActiveRecord::Schema.define(version: 20150202052428) do
 
   create_table "admins", force: true do |t|
     t.string   "email",              default: "", null: false
@@ -207,8 +207,12 @@ ActiveRecord::Schema.define(version: 20150129063221) do
     t.string   "username"
     t.string   "provider"
     t.string   "uid"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
